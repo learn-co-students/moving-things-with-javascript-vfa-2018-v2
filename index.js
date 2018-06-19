32f2ba9823beb1574b4827842b4f93282b3bcb34
James Peacock <james.hoke.peacock@gmail.com>
2018-06-19 16:29:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var right = parseInt(leftNumbers, 10) + 40
+ 
+  if (right < 400) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
