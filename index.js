34c0cfde365e07ce364e10e898d2d33d09dc0bf8
William McFadden <liam.e.mcfadden@gmail.com>
2018-06-21 21:17:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
+  var rightNumbers = dodger.style.left.replace('px', '')
   var right = parseInt(righttNumbers, 10)
  
   if (right > 0) {
-    dodger.style.right = `${right + 1}px`
+    dodger.style.left = `${right + 1}px`
   }
 }
 
 document.addEventListener('keydown', function(e) {
+ console.log(e.which)
   if (e.which === 39) {
     moveDodgerRight()
   }
