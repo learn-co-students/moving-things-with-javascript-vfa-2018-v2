3093d973192786059e1ae2fe203564f5f77dd4b9
Jin Zhang <jnfzhang1995@gmail.com>
2018-06-29 09:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ document.addEventListener('keydown', function(e) {
   }
 })
 var dodger = document.getElementById('dodger')
-
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
 document.addEventListener('keydown', function(e) {
   if (e.which === 39) {
     moveDodgerRight()
