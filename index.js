c3947c38ab5d76afba6513f530d367fd48e36136
Jin Zhang <jnfzhang1995@gmail.com>
2018-06-29 09:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ var dodger = document.getElementById('dodger')
 document.addEventListener('keydown', function(e) {
   console.log(e.which)
 })
+document.addEventListener('keydown', function(e) {
+  if (e.which === 39) {
+    var rightNumbers = dodger.style.right.replace('px', '')
+    var right = parseInt(rightNumbers, 10)
+ 
+    dodger.style.right = `${right - 1}px`
+  }
+})
